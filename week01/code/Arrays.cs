5db25bbdff4a3dfb166574a32c0b5b033b2a5152
Figuelia98 <figueliayasin98@gmail.com>
2025-07-06 00:30:09 +0300

    Dev week01 Done

---
+++ week01/code/Arrays.cs
@@ -0,0 +1,59 @@
+public static class Arrays
+{
+    
+      public static void Run()
+    {
+        double[] multiples = MultiplesOf(7, 5);
+        Console.WriteLine(string.Join(", ", multiples));
+    }
+    /// <summary>
+    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    public static double[] MultiplesOf(double number, int length)
+    {
+        // TODO Problem 1
+        //initiate a list
+        List<double> result = new List<double>();
+        //iterate base on length
+        for (var i = 1; i <= length; i++)
+        {  // compute multiple of number and append to the list
+            result.Add(i * number);
+        }
+        return result.ToArray(); // convert to array
+    }
+
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
+    ///
+    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
+    /// </summary>
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start
+        // Remember: Using comments in your program, write down your process for solving this problem
+        // step by step before you write the code. The plan should be clear enough that it could
+        // be implemented by another person.
+
+        // Initiated list
+        List<int> newlist = new List<int>();
+        //set rotated list part1
+        for (var i = data.Count-amount ; i < data.Count; i++)
+        {  
+            newlist.Add(data[i]);
+        }
+         //set ratoted list part2
+        for (var i = 0; i < (data.Count - amount); i++)
+        {
+            newlist.Add(data[i]);
+        }
+
+        data.Clear();
+        //Set new rotated list
+        data.AddRange(newlist);
+    }
+}
