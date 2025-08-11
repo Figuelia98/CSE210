a3a1f8a6e798776a687d8c91c03539d0aa94dce2
Figuelia98 <figueliayasin98@gmail.com>
2025-08-11 22:19:08 +0300

    Dev 06 Done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,19 +12,30 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        
+        if (value == Data)
+        return;
+
 
         if (value < Data)
         {
             // Insert to the left
-            if (Left is null)
+            if (Left is null && Left != new Node(value))
+            {
+
                 Left = new Node(value);
+            }
+
             else
+            {
                 Left.Insert(value);
+            }
+
         }
         else
         {
             // Insert to the right
-            if (Right is null)
+            if (Right is null && Right != new Node(value))
                 Right = new Node(value);
             else
                 Right.Insert(value);
@@ -34,12 +45,24 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+    if (value == Data)
+        return true;
+
+    if (value < Data)
+    {
+        return Left != null && Left.Contains(value);
+    }
+    else 
+    {
+        return Right != null && Right.Contains(value);
+    }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+    int lHeight = Left?.GetHeight() ?? 0;
+    int rHeight = Right?.GetHeight() ?? 0;
+
+    return 1 + Math.Max(lHeight, rHeight);// Replace this line with the correct return statement(s)
     }
 }
